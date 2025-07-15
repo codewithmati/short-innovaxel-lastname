4c7bacebdfdf3de350d5374636715898f006efae
codewithmati <m4mati10@gmail.com>
2025-07-16 01:55:28 +0500

    apply onclick function and also fetch data from user to send in database

--- url-shortener/app/page.tsx
+++ url-shortener/app/page.tsx
@@ -1,6 +1,16 @@
-import React from 'react'
+"use client";
+import { useState } from 'react'
 
 const page = () => {
+  const [url, setUrl] = useState("");
+  const handleSubmit = async () => {
+    const res = await fetch("/api/shorten", {
+      method: "POST",
+      body: JSON.stringify({ longUrl: url }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+  };
   return (
     <>
       <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
@@ -8,8 +18,10 @@ const page = () => {
       <input
         className="border p-2 w-full max-w-md mb-4"
         placeholder="Enter long URL"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
       />
-      <button className="bg-blue-500 text-white px-4 py-2 rounded">
+      <button onClick={handleSubmit} className="bg-blue-500 text-white px-4 py-2 rounded">
         Shorten
       </button>
         <p className="mt-4 text-green-600">
