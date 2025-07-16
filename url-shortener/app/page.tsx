b556b799841e78f37ca364ebc5ebd1d1cac09b26
codewithmati <m4mati10@gmail.com>
2025-07-16 14:04:37 +0500

    some editing in  frontend

--- url-shortener/app/page.tsx
+++ url-shortener/app/page.tsx
@@ -50,7 +50,7 @@ const Page = () => {
 
       <button
         onClick={handleSubmit}
-        className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded"
+        className="bg-blue-600 hover:bg-blue-700 text-white px-4 cursor-pointer py-2 rounded"
       >
         Shorten
       </button>
